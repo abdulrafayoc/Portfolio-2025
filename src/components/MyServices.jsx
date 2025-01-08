2d433f96c88adf84a1caaf8653247a419d3a6809
Abdul Rafay <abdulrafay5145593@gmial.com>
2025-01-08 23:51:12 +0500

    Did landing responsiveness made services page

---
+++ src/components/MyServices.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./MyServices.css";
+
+
+const MyServices = () => {
+
+return (
+    <div className="services-container">
+
+      <div className="vertical-text">
+        Just About
+      </div>
+
+      <div className="services">
+        <p className="service">Web Design</p>
+        <p className="service">Front-End Development</p>
+        <p className="service">Mobile Design</p>
+        <p className="service">User Interface (UI)</p>
+        <p className="service">User Experience (UX) Design</p>
+        <p className="service">Brand Identity Creation</p>
+      </div>
+    </div>
+  );
+
+};
+
+export default MyServices;
